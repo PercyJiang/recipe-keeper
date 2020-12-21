076ee3e951e909da1ddb44d8e9e49c85d3d0d833
PercyJiang <hjiang@wpi.edu>
2020-12-20 21:17:09 -0500

    finish

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { AppBar, Typography, InputBase, Toolbar } from '@material-ui/core'
-import SearchIcon from '@material-ui/icons/Search';
+import { AppBar, Typography, Toolbar } from '@material-ui/core'
 import useStyles from '../styles/header'
 import CreateDialog from './CreateDialog'
 
@@ -11,19 +10,6 @@ const Header = ({ currentId, setCurrentId }) => {
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
                 <Typography className={classes.title} variant="h6" noWrap>Recipe Keeper</Typography>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon />
-                    </div>
-                    <InputBase
-                        placeholder="Search…"
-                        classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'search' }}
-                    />
-                </div>
                 <div className={classes.grow} />
                 <CreateDialog currentId={currentId} setCurrentId={setCurrentId} />
             </Toolbar>
--- client/src/styles/header.js
+++ client/src/styles/header.js
@@ -1,36 +1,14 @@
-import { fade, makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles((theme) => ({
     grow: { flexGrow: 1, },
     title: {
+        fontWeight: 'bold',
+        fontSize: '30px',
         display: 'block',
         [theme.breakpoints.up('sm')]: {
             display: 'block',
         },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     inputRoot: {
         color: 'inherit',
